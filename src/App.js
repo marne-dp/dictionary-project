ee27cf629d5fa4d8b1e0afb13c30e890108a958a
marne-dp <104077297+marne-dp@users.noreply.github.com>
2022-08-31 15:03:17 +0200

    Added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">Hello</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <small>Coded by Marné du Preez </small>
+      </footer>
     </div>
   );
 }
